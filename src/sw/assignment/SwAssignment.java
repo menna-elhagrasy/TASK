787f30edb28c5bc919ad13c17e793bc1353f834c
kawther essam <kawther.esam14@gmail.com>
2017-03-25 22:12:23 +0200

    first commit

---
+++ src/sw/assignment/SwAssignment.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sw.assignment;
+
+/**
+ *
+ * @author Kawther Esam
+ */
+public class SwAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    int x=0;
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
